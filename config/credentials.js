8818d847c71746335ac08af9c20a3ceac4b0de38
Saurav Suman <sauravsuman0199@gmail.com>
2024-04-05 18:48:49 +0530

    secret key

--- config/credentials.js
+++ config/credentials.js
@@ -1,4 +1,4 @@
-const SECRET_PASS = process.env.PASS||'xsmtpsib-084da5b141c7165dea4d6ce916b0e42f73bdba2fa1c4b546d4dceb6275f9b3ff-rU9EsTzYLfapBCKj';
- const USER_EMAIL = process.env.USER||'sauravsuman0199@gmail.com ';
+const SECRET_PASS = process.env.PASS;
+ const USER_EMAIL = process.env.USER;
 
 module.exports = {USER_EMAIL,SECRET_PASS}
