c0f7c5295b4334b42870afae1f17f684d438d436
Saurav Suman <sauravsuman0199@gmail.com>
2024-04-08 23:31:16 +0530

    updated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 1337
+const PORT = 1337;
 
 app.listen(PORT,()=>{
     console.log(`server is running at http://localhost:${PORT}`);
